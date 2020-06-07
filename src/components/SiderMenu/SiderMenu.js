ee071320a893a1a7698f87f4e6c4d1310596fdd9
蔡金锋 <jeff.cai@99bill.com>
2020-06-07 14:52:17 +0800

    支持展开多个菜单项

--- src/components/SiderMenu/SiderMenu.js
+++ src/components/SiderMenu/SiderMenu.js
@@ -27,21 +27,9 @@ export default class SiderMenu extends PureComponent {
     return null;
   }
 
-  isMainMenu = key => {
-    const { menuData } = this.props;
-    return menuData.some(item => {
-      if (key) {
-        return item.key === key || item.path === key;
-      }
-      return false;
-    });
-  };
-
   handleOpenChange = openKeys => {
-    const moreThanOne =
-      openKeys.filter(openKey => this.isMainMenu(openKey)).length > 1;
     this.setState({
-      openKeys: moreThanOne ? [openKeys.pop()] : [...openKeys]
+      openKeys
     });
   };
 
